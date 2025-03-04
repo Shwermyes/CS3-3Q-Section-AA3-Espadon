3fc9777b4120d16d80f687069b159742290ae294
Shwermyes <ssespadon22@src.pshs.edu.ph>
2025-03-04 19:23:52 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-function PopulationGrowth(){
-	   
-    let initialpopulation = parseFloat(prompt('Enter the initial population '));
-    let rateofgrowth = parseFloat(prompt('Enter the rate of growth: '));
-    let timeinhours = parseFloat(prompt('Enter the time in hours: '));
-                  
-    let final = Math.round(initialpopulation*Math.pow(Math.E, (rateofgrowth*timeinhours)));
-                     
-    let Location = prompt('Enter the region or location of the monster: ');
-    let nameofmonster = prompt('Enter the name of the monster: ');
-    
-    let nameandlocation = Location.concat(" ", nameofmonster).toUpperCase();
-                     
-     document.getElementById("result").innerHTML = "After " + timeinhours + " hours, the population of monster " + nameandlocation  + "  increased to " + final;
+function Message() {
+    let message = prompt("Enter your message to verify:");
 
+    if (!message) {
+        document.getElementById("result").innerText = "No message entered.";
+        return;
+    }
+
+    let FR = "FR" || "fr";  
+    let AI =  "AI";  
+    let Ai = "Ai" || "aI";  
+    
+    let resultText = "";  
+    
+    if (message.indexOf("FR")!==-1) {  
+        resultText = ` The message ` + message + ` `  + `is legitimate.`;  
+    } else if (message.indexOf("AI")!==-1) {  
+        resultText = `The message ` + message + ` ` + ` is tampered with `  
+    } else if (message.indexOf("aI")!==-1||message.indexOf("Ai")!==-1) {  
+        resultText = `The message ` + message + `` + ` is tampered with.`;  
+    } else {  
+        resultText = `The message` + message + `` + ` is yet to be encoded.`;  
+    }  
+    document.getElementById("result").innerText = resultText;
 }
